cbc0ed47eb74c147119d44de527a8e201641ba14
unknown <admaxelfranco@protonmail.com>
2021-12-05 14:40:16 -0300

    Desarrollo ComponentsHome /Home/item/itemProducts => Declaracion de productos

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Home from "./components/componentsHome/Home";
+import Item from "./components/componentsHome/Item";
+
 /**
  * First we will load all of this project's JavaScript dependencies which
  * includes React and other helpers. It's a great starting point while
@@ -12,4 +18,13 @@ require('./bootstrap');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-require('./components/Example');
+
+
+ ReactDOM.render(
+  <React.StrictMode>
+      <Home/>
+  </React.StrictMode>,
+  document.getElementById("home")
+);
+
+
---
+++ resources/views/product.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
+
+    <title>Product / WebShopping</title>
+
+</head>
+
+<body>
+
+  <header></header>
+  
+ 
+  <section>
+    <article>
+      <div id="item"></div>
+    </article>
+  </section>
+ 
+
+  <footer></footer>
+
+
+  
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"
+        integrity="sha384-eMNCOe7tC1doHpGoWe/6oMVemdAVTMs2xqW4mwXrXsW0L84Iytr2wi5v2QjrP/xp"
+        crossorigin="anonymous"></script>   
+        <script src="{{ asset('js/app.js') }}" defer></script>
+</body>
+</html>
