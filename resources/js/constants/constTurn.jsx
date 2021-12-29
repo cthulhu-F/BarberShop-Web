a25ec2d03f768b5c321a0ec91ee3122dd3c21eeb
= <=>
2021-12-29 15:19:21 -0300

    FrancoLobos CherryPick  /turnDashboardFixed

--- resources/js/constants/constTurn.jsx
+++ resources/js/constants/constTurn.jsx
@@ -101,6 +101,66 @@ export const ITEM_TURNS = {
       time: "08:00",
       update_at: "14/12/2021", 
       status: "ACTIVE"
+    },
+    {
+      id: 2,
+      name: "Silla 1",
+      name_client: "Axel",
+      email_client: "maria@gmail.com", 
+      phone_client: "+54 9 362 459-4434", 
+      date: "18/01/2022",
+      time: "09:00",
+      turn_duration: "00:30",
+      update_at: "14/12/2021", 
+      status: "ACTIVE"
+    },
+    {
+      id: 3,
+      name: "Silla 4",
+      name_client: "Florencia",
+      email_client: "florencia@gmail.com", 
+      phone_client: "0909097867", 
+      date: "14/01/2022",
+      time: "10:00",
+      turn_duration: "01:15",
+      update_at: "14/12/2021", 
+      status: "NONACTIVE"
+    },
+    {
+      id: 4,
+      name: "Silla 3",
+      name_client: "martiin",
+      email_client: "martiin@gmail.com", 
+      phone_client: "45237658", 
+      date: "29/12/2021",
+      time: "11:00",
+      turn_duration: "01:00",
+      update_at: "14/12/2021", 
+      status: "NONACTIVE"
+    },
+    {
+      id: 5,
+      name: "Silla 3",
+      name_client: "santiago",
+      email_client: "santiago@gmail.com", 
+      phone_client: "123543092486'", 
+      date: "29/12/2021",
+      time: "12:00",
+      turn_duration: "00:45",
+      update_at: "24/12/2021", 
+      status: "CONFIRMED"
+    },
+    {
+      id: 6,
+      name: "Silla 2",
+      name_client: "ruperto",
+      email_client: "ruperto@gmail.com", 
+      phone_client: "123578999'", 
+      date: "29/12/2021",
+      time: "12:00",
+      turn_duration: "00:15",
+      update_at: "24/12/2021", 
+      status: "ACTIVE"
     }
   ]
 
---
+++ src/reducers/backofficeTurnDashboardReducer.js
@@ -0,0 +1,180 @@
+import { BACKOFFICE_TURN_DASHBOARD_TYPES } from '../actions/backofficeTurnDashboardActions';
+import { ITEM_TURNS } from '../../resources/js/constants/constTurn';
+import "../../resources/css/sweetAlert.css"
+import swal from 'sweetalert2';
+
+
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+var yyyy = today.getFullYear();
+var todayString =  dd +'/'+ mm + '/'+yyyy;
+
+
+// export const InitialTurns ={
+//     allShcheduled : ITEM_TURNS.orderTurns,
+//     todayScheduled : ITEM_TURNS.orderTurns.filter((turn)=>turn.date == todayString),
+//     tomorrowScheduled : ITEM_TURNS.orderTurns.filter((turn)=>turn.date != todayString),
+//     dashboardTurns : ITEM_TURNS.orderTurns.filter((turn)=>turn.date == todayString),
+// }
+
+const sortByDate = (nonSortedTurns) =>{
+    const sortedTurns =[];
+    let sortedOrder = [];
+    nonSortedTurns.map((turn)=>
+        sortedOrder.push(
+            {order : turn.date.split('/')[2]+turn.date.split('/')[1]+turn.date.split('/')[0]+turn.time.split(':')[0]+turn.time.split(':')[1], id: turn.id}
+        )
+    )
+    sortedOrder.sort(function(a, b) {
+        return a.order - b.order;
+    });
+
+    sortedOrder.map(sortedId=>
+        sortedTurns.push(
+            ...nonSortedTurns.filter(turn => turn.id == sortedId.id)
+        )
+    )
+
+    return sortedTurns
+}
+
+const filteredByActive = (nonFilteredTurns) =>{
+    return nonFilteredTurns.filter(turn => turn.status =="ACTIVE")
+}
+
+export const FilteredTurns ={
+    allShcheduled : sortByDate(ITEM_TURNS.orderTurns),
+    todayScheduled : filteredByActive(sortByDate(ITEM_TURNS.orderTurns.filter((turn)=>turn.date == todayString))),
+    tomorrowScheduled : filteredByActive(sortByDate(ITEM_TURNS.orderTurns.filter((turn)=>turn.date != todayString))),
+    dashboardTurns : sortByDate(ITEM_TURNS.orderTurns),
+}
+
+export function backofficeTurnDashboardReducer(state, action){
+    switch(action.type){ 
+        case BACKOFFICE_TURN_DASHBOARD_TYPES.RESET_INITIAL_STATE :{
+            return allShcheduled
+        }
+
+        // case BACKOFFICE_TURN_DASHBOARD_TYPES.GET_TODAYS_SCHEDULE :{           
+        //     return {...state, dashboardTurns : state.todayScheduled};
+        // }
+
+
+        // case BACKOFFICE_TURN_DASHBOARD_TYPES.GET_TOMOORROW_SCHEDULE :{
+        //     return {...state, dashboardTurns : state.tomorrowScheduled};
+        // }
+
+        case BACKOFFICE_TURN_DASHBOARD_TYPES.FILTER_DASHBOARD:{
+            let dateMin = action.min.split('-')
+            let dateYyyy = dateMin[0];
+            let dateMm = dateMin[1];
+            let dateDd = dateMin[2];
+            let compartaiveMin = parseInt(dateYyyy+dateMm+dateDd)
+
+            let dateMax = action.max.split('-')
+            let dateYyyyMax = dateMax[0];
+            let dateMmMax = dateMax[1];
+            let dateDdMax = dateMax[2];
+            let compartaiveMax = parseInt(dateYyyyMax+dateMmMax+dateDdMax)
+            console.log(compartaiveMin)
+            console.log(compartaiveMax)
+            console.log(action.chairName)
+
+            let filteredByName;
+
+            let FilteredBydate =  state.allShcheduled.filter(turn =>
+                    parseInt(turn.date.split('/')[2]+turn.date.split('/')[1]+turn.date.split('/')[0]) >= compartaiveMin && 
+                    parseInt(turn.date.split('/')[2]+turn.date.split('/')[1]+turn.date.split('/')[0]) <= compartaiveMax 
+            );
+
+
+            if(action.chairName != "Ver silla"){
+                filteredByName = FilteredBydate.filter(turn=>
+                    turn.name == action.chairName
+                )
+            }else {
+                filteredByName = FilteredBydate
+            }
+            
+            return {...state, dashboardTurns : filteredByName};
+
+        }
+
+        case BACKOFFICE_TURN_DASHBOARD_TYPES.FILTER_BY_CHAIR :{
+            let filteredByChair
+            if(action.payload != "Ver silla"){
+                filteredByChair = state.allShcheduled.filter((turn)=>turn.name == action.payload);
+            }else {
+                filteredByChair = state.allShcheduled
+            }
+
+            return {...state, dashboardTurns : filteredByChair};
+        }
+
+
+        case BACKOFFICE_TURN_DASHBOARD_TYPES.EDITE_TURN_SCHEDULE :{
+            let allTurnsScheduled = state.allShcheduled;
+            let newEditableTurn = action.turn
+            let modified = false;
+
+            if (action.date != ""){
+                let newDay = action.date.split('-')
+                newEditableTurn.date = newDay[2] + '/' + newDay[1] +'/' +newDay[0]
+                modified = true;
+            }
+
+
+            if (action.time != ""){
+                newEditableTurn.time = action.time;
+                modified = true;
+
+            }
+
+            if (action.duration != ""){
+                newEditableTurn.turn_duration = action.duration;
+                modified = true;
+
+            }
+
+            if (modified){
+                newEditableTurn.update_at = todayString;
+                allTurnsScheduled = allTurnsScheduled.filter((turn)=>turn.id != action.turn.id);
+                allTurnsScheduled.push(newEditableTurn)
+                console.log(allTurnsScheduled)
+
+                let allScheduleSorted = sortByDate(allTurnsScheduled);
+                return {...state, allShcheduled: allScheduleSorted}
+            }
+
+            else{
+                return state
+            }
+        }
+
+        case BACKOFFICE_TURN_DASHBOARD_TYPES.ORDER_BY_DATE :{
+            return state
+        }
+
+        case BACKOFFICE_TURN_DASHBOARD_TYPES.SET_TURN_STATUS :{
+            let editableTurn = state.allShcheduled.find(turn=>turn.id == action.turnId);
+            let allTurnsScheduled = state.allShcheduled.filter(turn=>turn.id != action.turnId);
+            editableTurn.status = action.newStatus;
+
+            allTurnsScheduled.push(editableTurn)
+
+            let allScheduleSorted =sortByDate(allTurnsScheduled);
+            console.log(allScheduleSorted)
+            return {...state, allShcheduled: allScheduleSorted,
+                todayScheduled: filteredByActive(state.todayScheduled),
+                tomorrowScheduled: filteredByActive(state.tomorrowScheduled),
+                dashboardTurns: state.dashboardTurns,
+            }
+
+        }   
+
+        default :{
+            return state;
+        }
+    }
+}
