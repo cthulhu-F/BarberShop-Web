ef33e28337803bc5d085ef11668537b2f5cdb303
= <=>
2022-01-27 15:09:19 -0300

    francolobos/frontend/react/MetaTagsTesting

--- resources/js/components/componentsHome/Home.jsx
+++ resources/js/components/componentsHome/Home.jsx
@@ -2,7 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '../../../css/main.css';
 import { ITEM_IMG } from '../../constants/constImg';
 
-
+import { Helmet } from 'react-helmet';
 
 const Home = () => {
 
@@ -14,6 +14,16 @@ const Home = () => {
 
 
     <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="carousel">
+        <Helmet>
+          <title data-react-helmet="true" > Brothers Barbershop</title>
+          <meta name="description" content="
+            Vistá nuestra página y solicitá un turno 100% online. En Brothers Barbershop tambíen podés comprar tus productos favoritos.
+          "/>    
+          <meta name="keywords" content="
+            barbería, peluquería, cortes de cabello, cortes de barba, república dominicana, barbería online, productos de belleza, cuidado facial, cuidado del cabello
+          "/>
+          <meta http-equiv="expires" content="86400"/>    
+        </Helmet>
       <div className="carousel-indicators">
         <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
         <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
